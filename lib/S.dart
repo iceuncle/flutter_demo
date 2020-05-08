4da600d3917eff3b70e6435ff5f9ea14c08316b9
wutianyang <1582869895@qq.com>
2020-05-08 16:18:54 +0800

    修改屏幕适配类为单例类

--- lib/S.dart
+++ lib/S.dart
@@ -2,39 +2,43 @@ import 'dart:ui';
 
 class S {
   /// 设计稿标准 1080 x 1920 像素
-  static double _normalWidth = 360;
-  static double _normalHeight = 640;
-  static double _normalRatio = 3;
+  double _normalWidth = 360;
+  double _normalHeight = 640;
+  double _normalRatio = 3;
 
   // 是否以宽度为适配标准
-  static bool isBasedInWidth = true;
+  bool isBasedInWidth = true;
 
   // 真实设备和设计稿标准的换算比例
-  static double screenWidthRatio;
-  static double screenHeightRatio;
+  double screenWidthRatio;
+  double screenHeightRatio;
 
   // 设备像素比
-  static double _devicePixelRatio;
+  double _devicePixelRatio;
+
+  static S instance;
 
   static init() {
-    screenWidthRatio =
-        window.physicalSize.width / (_normalWidth * _normalRatio);
-    screenHeightRatio =
-        window.physicalSize.height / (_normalHeight * _normalRatio);
-    _devicePixelRatio = window.devicePixelRatio;
+    if (instance == null) instance = new S();
+    instance.screenWidthRatio = window.physicalSize.width /
+        (instance._normalWidth * instance._normalRatio);
+    instance.screenHeightRatio = window.physicalSize.height /
+        (instance._normalHeight * instance._normalRatio);
+    instance._devicePixelRatio = window.devicePixelRatio;
   }
 
   static double px(double number) {
+    return instance._px(number);
+  }
+
+  double _px(double number) {
     double results = 0.0;
     if (isBasedInWidth == true) {
-      results =
-          (number * _normalRatio) * screenWidthRatio / _devicePixelRatio;
+      results = (number * _normalRatio) * screenWidthRatio / _devicePixelRatio;
       print('传入的像素为：$number，转换后的逻辑像素为：$results');
       return results;
     } else {
-      results = (number * _normalRatio) *
-          screenHeightRatio /
-          _devicePixelRatio;
+      results = (number * _normalRatio) * screenHeightRatio / _devicePixelRatio;
       print('传入的像素为：$number，转换后的逻辑像素为：$results');
       return results;
     }
